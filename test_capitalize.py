d2f8847606b5680e1c84ad29fd80d20f86bf373c
Adam Zadrozny <adam@ius.ai>
2021-03-31 12:40:53 +0200

    Modifications to basic test

--- test_capitalize.py
+++ test_capitalize.py
@@ -1,6 +1,6 @@
 # test_capitalize.py
-
 import pytest
+
 
 def capital_case(x):
     return x.capitalize()
@@ -9,5 +9,5 @@ def test_capital_case():
     assert capital_case('semaphore') == 'Semaphore'
 
 def test_raises_exception_on_non_string_arguments():
-    with pytest.raises(TypeError):
+    with pytest.raises(AttributeError): # TypeError AttributeError
         capital_case(9)
